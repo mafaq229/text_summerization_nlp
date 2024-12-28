26ea20c5de1ce8fc86059475ed0d8637d4752b03
Muhammad Afaq <muhammadafaq1999@gmail.com>
2024-12-29 00:14:34 +0400

    Data Transformation

--- src/textsummarizer/config/configuration.py
+++ src/textsummarizer/config/configuration.py
@@ -1,7 +1,7 @@
 from src.textsummarizer.constants import *
 from src.textsummarizer.utils.common import read_yaml, create_directories
 
-from src.textsummarizer.entity import DataIngestionConfig # ,ModelEvaluationConfig,DataTransformationConfig,ModelTrainerConfig
+from src.textsummarizer.entity import DataIngestionConfig, DataTransformationConfig # ,ModelEvaluationConfig,DataTransformationConfig,ModelTrainerConfig
 
 class ConfigurationManager:
     def __init__(self,
@@ -24,4 +24,18 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+    
+    def get_data_transformation_config(self) -> DataTransformationConfig:
+        config = self.config.data_transformation
+        create_directories([config.root_dir])
+        
+        data_transformation_config = DataTransformationConfig(
+            root_dir=config.root_dir,
+            data_path=config.data_path,
+            tokenizer_name=config.tokenizer_name
+        )
+        return data_transformation_config
+    
+    
+    
 
